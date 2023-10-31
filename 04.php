e2f49835d4c4d0c3428cf1b07f135183eaae882d
samucaavant19 <141763952+samucaavant19@users.noreply.github.com>
2023-10-31 14:20:16 -0400

    Add files via upload

---
+++ 04.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Brincadeira</title>
+</head>
+
+<body>
+    <?php 
+    $campus = "Instituto Federal";
+
+    echo "<p> Eu estudo no $campus! </p>";
+    ?>
+
+</body>
+</html>
